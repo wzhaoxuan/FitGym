c09828c8f2b66524e952a7f6f69700d666c16a5c
wzhaoxuan <newzxuan213@gmail.com>
2024-12-08 19:13:25 +0800

    Create class and apply monad

--- Main.hs
+++ Main.hs
@@ -7,11 +7,27 @@ validPassword = ["user123", "coach123"]
 
 -- Define the Email type
 type Email = String
+type Password = String
+
 -- Define the UserType data type
-data UserType = User Email | Coach Email
+data UserType = User Email Password | Coach Email Password deriving (Show)
+
+-- Define a class for loginable types
+class Loginable a where
+    login :: a -> String -> String -> Maybe String
+
+-- Implement the Loginable instance for UserType
+instance Loginable UserType where
+       login (User email password) enteredEmail enteredPassword
+        | email == enteredEmail && password == enteredPassword = Just "Login successful. Welcome User!"
+        | otherwise = Nothing
+       login (Coach email password) enteredEmail enteredPassword
+        | email == enteredEmail && password == enteredPassword = Just "Login successful. Welcome Coach!"
+        | otherwise = Nothing
+
 
 -- Function to perform login
-performLogin :: (String -> String -> Maybe UserType) -> IO()
+performLogin :: (String -> String -> Maybe String) -> IO()
 performLogin validCredentials = do
        putStrLn "\n***************************************"
        putStrLn "     Welcome to the FitGym System      "
@@ -22,26 +38,25 @@ performLogin validCredentials = do
        password <- getLine
        -- Apply the validation function and handle the result using a Functor
        case validCredentials email password of
-              Just userType -> putStrLn ("Login successful. Welcome " ++ showUserType userType ++ "!")
+              Just successLogin -> putStrLn successLogin
               Nothing -> do
                      putStrLn "Invalid credentials. Please try again."
-                     performLogin validCredentials 
+                     performLogin validCredentials
        
 -- Function to display the Usertype 
 showUserType :: UserType -> String
-showUserType (User _)= "User"
-showUserType (Coach _)= "Coach"
+showUserType (User _ _) = "User"
+showUserType (Coach _ _) = "Coach"
 
 -- Validate credentials and return the appropriate Usertype
-validCredentials :: String -> String -> Maybe UserType
+validCredentials :: String -> String -> Maybe String
 validCredentials email password = 
        -- Zip the email and password lists and lookup the email
        case lookup email (zip validEmail validPassword) of
               Just correctPassword | correctPassword == password -> 
-                     -- Determine whether the user is a User or Coach
                      if email == "user@fitgym.com" 
-                     then Just (User email)  -- Return User type for regular user
-                     else Just (Coach email)  -- Return Coach type for coach
+                     then Just (User email password) >>= \user -> login user email password -- Return User type for user
+                     else Just (Coach email password) >>= \coach -> login coach email password -- Return Coach type for coach
               _ -> Nothing
 
 -- Function to display the login menu and get user choice
@@ -61,7 +76,7 @@ main :: IO ()
 main = do
     choice <- getChoice
     case choice of
-        1 -> performLogin validCredentials
+        1 -> performLogin validCredentials  
         2 -> putStrLn "Exiting the system. Goodbye!"
         _ -> do
             putStrLn "Invalid choice. Please try again.\n"

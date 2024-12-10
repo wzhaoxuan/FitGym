2c887e434ab00cc0b00246d5bc8046e8bec1a8b0
wzhaoxuan <newzxuan213@gmail.com>
2024-12-10 14:23:58 +0800

    Ask Experience only for user

--- Main.hs
+++ Main.hs
@@ -30,9 +30,23 @@ instance Loginable UserType where
         | email == enteredEmail && password == enteredPassword = Just $ "Login successful. Welcome coach: " ++ email ++ "!"
         | otherwise = Nothing
 
+-- Function to display the Usertype 
+showUserType :: UserType -> String
+showUserType (User _ _) = "User"
+showUserType (Coach _ _) = "Coach"
+
+-- Validate credentials and return the appropriate Usertype
+validCredentials :: String -> String -> Maybe UserType
+validCredentials email password = 
+       -- Zip the email and password lists and lookup the email
+       case lookup email (zip validUserEmail validUserPassword) of
+              Just correctPassword | correctPassword == password ->  Just (User email password)  -- Return User type for user
+              _ -> case lookup email (zip validCoachEmail validCoachPassword) of
+                     Just correctPassword | correctPassword == password -> Just (Coach email password)  -- Return Coach type for coach 
+                     _ -> Nothing
 
 -- Function to perform login
-performLogin :: (String -> String -> Maybe String) -> IO()
+performLogin :: (String -> String -> Maybe UserType) -> IO()
 performLogin validCredentials = do
        putStrLn "\n***************************************"
        putStrLn "     Welcome to the FitGym System      "
@@ -43,27 +57,14 @@ performLogin validCredentials = do
        password <- getLine
        -- Apply the validation function and handle the result using a Functor
        case validCredentials email password of
-              Just successLogin -> putStrLn successLogin >> userJourney
+              Just userType -> do
+                     case userType of
+                            User _ _ -> userJourney
+                            Coach _ _ -> putStrLn "You are logged in as a coach."
               Nothing -> do
                      putStrLn "Invalid credentials. Please try again."
                      performLogin validCredentials
-       
--- Function to display the Usertype 
-showUserType :: UserType -> String
-showUserType (User _ _) = "User"
-showUserType (Coach _ _) = "Coach"
 
--- Validate credentials and return the appropriate Usertype
-validCredentials :: String -> String -> Maybe String
-validCredentials email password = 
-       -- Zip the email and password lists and lookup the email
-       case lookup email (zip validUserEmail validUserPassword) of
-              Just correctPassword | correctPassword == password -> 
-                     Just (User email password) >>= \user -> login user email password -- Return User type for user
-              _ -> case lookup email (zip validCoachEmail validCoachPassword) of
-                     Just correctPassword | correctPassword == password ->
-                            Just (Coach email password) >>= \coach -> login coach email password -- Return Coach type for coach 
-                     _ -> Nothing
 
 -- Function to ask User experience at Gym after login
 askUserExperience :: IO String
@@ -74,8 +75,8 @@ askUserExperience =
        putStrLn "2. Intermediate (Been at the gym. Already worked ouut for a few months)" >>
        putStrLn "3. Advanced (Equiments are my freinds)" >>
        putStr "Enter your choice: " >>
-       getLine >>= \choice ->
-              case choice of 
+       getLine >>= \experience ->
+              case experience of 
                      "1" -> return "Beginner"
                      "2" -> return "Intermediate"
                      "3" -> return "Advanced"
@@ -90,12 +91,12 @@ userJourney = askUserExperience >>= \experience ->
 -- Function to display the login menu and get user choice
 getChoice :: IO Int
 getChoice = do
-    putStrLn "***************************************"
-    putStrLn "     Welcome to the FitGym System      "
-    putStrLn "***************************************"
-    putStrLn "1. Login"
-    putStrLn "2. Exit"
-    putStr "Enter your choice: "
+    putStrLn "***************************************" 
+    putStrLn "     Welcome to the FitGym System      " 
+    putStrLn "***************************************" 
+    putStrLn "1. Login" 
+    putStrLn "2. Exit" 
+    putStr "Enter your choice: " 
     choice <- getLine
     return (read choice :: Int)
 
